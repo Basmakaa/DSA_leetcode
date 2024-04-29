10e2638f7a161dee195fa3bc1bb0b34f02839b8b
BK <58495262+Basmakaa@users.noreply.github.com>
2024-04-29 15:08:57 -0400

    added solution with no string conversion

--- Palindrom-number problem.py
+++ Palindrom-number problem.py
@@ -15,7 +15,34 @@ class Solution(object):
                 return False
             i+=1 
             result = True
-
-
         return result 
+
+
+#Solution without converting to string 
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        result = False 
+        if x<0 :
+            return False
+        if x<10 and x>-1 :
+            return True
+        mylist = []
+        while x :
+            #derive the number so we get each single difit without converting to string 
+            x, digit = divmod(x, 10)
+            mylist.append(digit)
+        print(mylist)
+        i = 0
+        size = len(mylist)
+        while i<size :
+            if mylist[i] != mylist[size -1 - i] :
+                return False
+            i+=1 
+            result = True
+        return result 
+        
         
