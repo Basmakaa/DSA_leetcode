8fb349a39f289270bc9e7816f088a2828d634f52
BK <58495262+Basmakaa@users.noreply.github.com>
2024-08-28 19:06:03 -0400

    Create LRUCache_Hashmap.py

---
+++ LRUCache_Hashmap.py
@@ -0,0 +1,24 @@
+from collections import OrderedDict
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        else:
+            # Move the accessed key to the end to mark it as recently used
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            # Update the value and move the key to the end
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            # Pop the first item (least recently used)
+            self.cache.popitem(last=False)
