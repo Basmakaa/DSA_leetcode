08ddedfadadb4d95e5fa7bde76e7f17fd3fb9490
BK <58495262+Basmakaa@users.noreply.github.com>
2024-09-14 17:21:57 -0400

    Create countComponents_UnionFind.py

---
+++ countComponents_UnionFind.py
@@ -0,0 +1,50 @@
+class UnionFind(object):
+    def __init__(self, n):
+        self.par = {}
+        self.rank = {}
+
+        for i in range(n ):
+            self.par[i] = i
+            self.rank[i] = 0
+    
+    # Find parent of n, with path compression.
+    def find(self, n):
+        p = self.par[n]
+        while p != self.par[p]:
+            self.par[p] = self.par[self.par[p]]
+            p = self.par[p]
+        return p
+
+    # Union by height / rank.
+    # Return false if already connected, true otherwise.
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2:
+            return False
+        
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+        elif self.rank[p1] < self.rank[p2]:
+            self.par[p1] = p2
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += 1
+        return True
+
+class Solution(object):
+    def countComponents(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        uf = UnionFind(n)
+        res = n
+        for e in edges:
+            num = uf.union(e[0], e[1])
+            if num == True:
+                res -=1 
+        return res
+
+
+        
