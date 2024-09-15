980c66ec3cb102d261139bdb87c1695d7279f33b
BK <58495262+Basmakaa@users.noreply.github.com>
2024-09-15 15:13:49 -0400

    Create preorderTraversal_iterativeDFS.py

---
+++ preorderTraversal_iterativeDFS.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+
+        stack = []
+        curr = root
+        res =[]
+
+        while curr or stack :
+            if curr:
+                res.append(curr.val)
+                if curr.right:
+                    stack.append(curr.right)
+                curr = curr.left
+            else :
+                curr = stack.pop()
+        return res
+
+
+       
