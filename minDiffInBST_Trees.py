a71264b596b792167363010d4b36f0ae80f27338
BK <58495262+Basmakaa@users.noreply.github.com>
2024-10-01 22:03:45 -0400

    Create minDiffInBST_Trees.py

---
+++ minDiffInBST_Trees.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minDiffInBST(self, root):
+
+        self.prev = None
+        self.min_diff = float('inf')
+
+        def inorder(node):
+            if node == None:
+                return
+            
+            inorder(node.left)
+
+            if self.prev is not None:
+                self.min_diff = min(self.min_diff, node.val - self.prev)
+
+            self.prev = node.val
+
+            inorder(node.right)
+    
+        inorder(root)
+        return self.min_diff
+
+        
